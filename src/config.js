b988a163b8f5e50fac93dbea9f6fdca5c0b4b39f
PM <81687913+pm83dev@users.noreply.github.com>
2024-11-15 14:54:27 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    NGROK_URL: 'https://9c27-31-171-141-197.ngrok-free.app',
+    NGROK_URL: 'https://d857-31-171-141-197.ngrok-free.app',
 };
 
 export default config;
