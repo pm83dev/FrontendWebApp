ca410416e94dbc9699a171a8717b9036c1cc2e76
PM <paolo.miccoli.83@gmail.com>
2024-12-03 13:16:13 +0100

    modifica url ngrok

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    NGROK_URL: 'https://c42f-31-171-141-197.ngrok-free.app',
+    NGROK_URL: 'https://34d7-31-171-141-197.ngrok-free.app',
     //NGROK_URL: 'https://localhost:7181',
 };
 
